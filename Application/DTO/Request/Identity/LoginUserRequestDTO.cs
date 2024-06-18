5deace8e767b498050df48c8f16f3f205324f7fa
Nigar <imnigarvalikhanova@gmail.com>
2024-06-18 16:05:27 +0400

    Logout

--- Application/DTO/Request/Identity/LoginUserRequestDTO.cs
+++ Application/DTO/Request/Identity/LoginUserRequestDTO.cs
@@ -13,7 +13,7 @@ namespace Application.DTO.Request.Identity
         [RegularExpression("[^@\\t\\r\\n]+@[^@\\t\\r\\n]+\\.[^@\\t\\r\\n]+", ErrorMessage ="Belə bir email ünvanı yoxdur!")]
         public string Email { get; set; }
         [Required]
-        [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#*!@$ %^&*-]).{8,}$", ErrorMessage ="Sizin şifrəniz böyük, kiçik hərflərdən və rəqəm və işarələrdən ibarət olmalıdır!")]
+        [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#*!@$ %^&*-]).{8,}$", ErrorMessage ="Şifrəniz yalnışdır!")]
         [MinLength(8), MaxLength(100)]
         public string Password { get; set; }
     }
